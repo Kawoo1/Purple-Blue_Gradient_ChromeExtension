517987b63d888e8544f662e0ebc04309a5504a90
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2023-11-04 02:24:14 -0700

    Debug Update popup.js
    Upon running extension, ran into a "Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')" error, a typerror, and needed to modify it to ensure all elements exist in the DOM.

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 //This document allows the BUTTON to function. Applies the actual Gradient
-document.getElementById('applyGradient').addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('applyGradient').addEventListener('click', () => {
     chrome.scripting.executeScript({
       target: { tabId: chrome.tabs.Tab.tabId },
       function: applyGradient
     });
   });
-  
+
   function applyGradient() {
     document.body.style.background = 'linear-gradient(to bottom, #8E44AD, #3498DB)';
   }
+});
