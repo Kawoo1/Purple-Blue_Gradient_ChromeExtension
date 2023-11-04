b4bebe2ecd408aef7cb765d0f3b283f9fff4afea
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2023-11-04 02:14:00 -0700

    popup.js
    Javascript file that adds the functionality of the gradient and making it an event.

---
+++ popup.js
@@ -0,0 +1,11 @@
+//This document allows the BUTTON to function. Applies the actual Gradient
+document.getElementById('applyGradient').addEventListener('click', () => {
+    chrome.scripting.executeScript({
+      target: { tabId: chrome.tabs.Tab.tabId },
+      function: applyGradient
+    });
+  });
+  
+  function applyGradient() {
+    document.body.style.background = 'linear-gradient(to bottom, #8E44AD, #3498DB)';
+  }
