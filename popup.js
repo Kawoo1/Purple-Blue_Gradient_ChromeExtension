715731b07aa2d4b8368b2e97e71b7f52bfb58a41
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2023-11-05 15:53:14 -0700

    Debug popup.js
    Adjusted popup.js to handle a "Type-error". (Errors still persist. Patching one by one)

--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 //This document allows the BUTTON to function. Applies the actual Gradient
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('applyGradient').addEventListener('click', () => {
-    chrome.scripting.executeScript({
-      target: { tabId: chrome.tabs.Tab.tabId },
-      function: applyGradient
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const activeTab = tabs[0];
+      chrome.runtime.sendMessage({ tabId: activeTab.id, action: "applyGradient" }, function(response) {
+        if (response === 'success') {
+          console.log('Gradient applied.');
+        }
+      });
     });
   });
-
-  function applyGradient() {
-    document.body.style.background = 'linear-gradient(to bottom, #8E44AD, #3498DB)';
-  }
 });
