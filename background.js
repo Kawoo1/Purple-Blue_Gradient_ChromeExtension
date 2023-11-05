a107b07ad43c99188af7ebb6cb15aba504feaff5
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2023-11-05 15:50:44 -0700

    Update background.js
    Modified background.js to handle a "Type-Error"

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 //This javascript file handles message passing.
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: applyGradient
-    });
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "applyGradient") {
+      chrome.scripting.executeScript({
+        target: { tabId: request.tabId },
+        function: applyGradient
+      });
+    }
   });
   
   function applyGradient() {
     document.body.style.background = 'linear-gradient(to bottom, #8E44AD, #3498DB)';
   }
-  
