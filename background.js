c2f7ef099d2dd866246e4b435c32398944a27b1c
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2023-11-05 16:24:11 -0700

    11/5 Update background.js
    Updated the background.js file to initialize the chrome.scripting API BEFORE any of my extensions code. chrome.scripting needs to be accessible before any of the other code and functions run to avoid the typerror.

--- background.js
+++ background.js
@@ -1,20 +1,22 @@
 //This javascript file handles message passing.
-//Updated to properly use the chrome.scripting API
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === "applyGradient") {
-    applyGradient(); // Execute your function to apply the gradient
-    sendResponse({ message: "Gradient applied successfully" });
+//Updated to properly use the chrome.scripting API, again.
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "applyGradient") {
+      applyGradient(); // Execute your function to apply the gradient
+      sendResponse({ message: "Gradient applied successfully" });
+    }
+  });
+
+  function applyGradient() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const activeTab = tabs[0];
+      chrome.scripting.executeScript({
+        target: { tabId: activeTab.id },
+        function: () => {
+          document.body.style.background = 'linear-gradient(to bottom, #8E44AD, #3498DB)';
+        }
+      });
+    });
   }
 });
-
-function applyGradient() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    const activeTab = tabs[0];
-    chrome.scripting.executeScript({
-      target: { tabId: activeTab.id },
-      function: () => {
-        document.body.style.background = 'linear-gradient(to bottom, #8E44AD, #3498DB)';
-      }
-    });
-  });
-}
