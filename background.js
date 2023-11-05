bae801fc69b745e4640f5019704daa9fba33d93f
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2023-11-05 16:07:01 -0700

    Update background.js
    Extended the applyGradient function to better incorporate "chrome.scripting" as well as modified the chrome.runtime event listener. Currently receiving a "runtime error" in extension simulations.

--- background.js
+++ background.js
@@ -1,12 +1,20 @@
 //This javascript file handles message passing.
 //Updated to properly use the chrome.scripting API
-chrome.action.onClicked.addListener((tab) => {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "applyGradient") {
+    applyGradient(); // Execute your function to apply the gradient
+    sendResponse({ message: "Gradient applied successfully" });
+  }
+});
+
+function applyGradient() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const activeTab = tabs[0];
     chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: applyGradient
+      target: { tabId: activeTab.id },
+      function: () => {
+        document.body.style.background = 'linear-gradient(to bottom, #8E44AD, #3498DB)';
+      }
     });
   });
-  
-  function applyGradient() {
-    document.body.style.background = 'linear-gradient(to bottom, #8E44AD, #3498DB)';
-  }
+}
