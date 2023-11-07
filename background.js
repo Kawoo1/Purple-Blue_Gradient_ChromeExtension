b745789900e730a9a252640a9a2db44adf848958
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2023-11-06 22:28:10 -0700

    11/6 Remove Gradient Update background.js
    Added an additional function, removeGradient, that when called removes the applied gradient.

--- background.js
+++ background.js
@@ -3,8 +3,11 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "applyGradient") {
-      applyGradient(); // Execute your function to apply the gradient
+      applyGradient();
       sendResponse({ message: "Gradient applied successfully" });
+    } else if (request.action === "removeGradient") {
+      removeGradient();
+      sendResponse({ message: "Gradient removed successfully" });
     }
   });
 
@@ -14,6 +17,17 @@ chrome.runtime.onInstalled.addListener(function() {
       chrome.tabs.executeScript(activeTab.id, {
         code: `
           document.body.style.background = 'linear-gradient(to bottom, #8E44AD, #3498DB)';
+        `
+      });
+    });
+  }
+
+  function removeGradient() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const activeTab = tabs[0];
+      chrome.tabs.executeScript(activeTab.id, {
+        code: `
+          document.body.style.background = 'initial';
         `
       });
     });
