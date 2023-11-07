07665f3f17c5c547866ad86c9ff29cd8dcb3dd28
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2023-11-06 22:26:51 -0700

    11/6 Button Update popup.js
    Added a functionality to the new button created in the popup.html document to remove the gradient when clicked.

--- popup.js
+++ popup.js
@@ -1,16 +1,36 @@
 //This document allows the BUTTON to function. Applies the actual Gradient
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('applyGradient').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      const activeTab = tabs[0];
-      chrome.runtime.sendMessage({ tabId: activeTab.id, action: "applyGradient" }, function(response) {
-        //Error check. If the gradient is applied correctly, console response that it passed. else, passes a console reponse that the gradient application failed.
-        if (response && response.message === 'Gradient applied successfully') {
-          console.log('Gradient applied successfully.');
-        } else {
-          console.error('Failed to apply the gradient.');
-        }
-      });
-    });
+    applyGradient();
+  });
+
+  document.getElementById('removeGradient').addEventListener('click', () => {
+    removeGradient();
   });
 });
+
+function applyGradient() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const activeTab = tabs[0];
+    chrome.runtime.sendMessage({ tabId: activeTab.id, action: "applyGradient" }, function(response) {
+      if (response && response.message === 'Gradient applied successfully') {
+        console.log('Gradient applied successfully.');
+      } else {
+        console.error('Failed to apply the gradient.');
+      }
+    });
+  });
+}
+
+function removeGradient() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const activeTab = tabs[0];
+    chrome.runtime.sendMessage({ tabId: activeTab.id, action: "removeGradient" }, function(response) {
+      if (response && response.message === 'Gradient removed successfully') {
+        console.log('Gradient removed successfully.');
+      } else {
+        console.error('Failed to remove the gradient.');
+      }
+    });
+  });
+}
